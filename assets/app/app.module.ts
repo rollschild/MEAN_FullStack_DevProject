ed0dd12a54e8c32fe00448fbd0a6e54ca23b9bb4
Guangchu Shi <rollschild@localhost.localdomain>
2018-02-26 15:57:15 -0500

    Add message-input

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -6,13 +6,17 @@ import { MessageComponent } from "./messages/message.component";
 import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
 import { MessageListComponent } from "./messages/message-list.component";
+import { MessageInputComponent } from "./messages/message-input.component";
+
+
 @NgModule({ // decorator?
     declarations: [
         AppComponent,
         // this is the place where you add new components, directives, or pipes
         // it's so called "register"
         MessageComponent,
-        MessageListComponent
+        MessageListComponent,
+        MessageInputComponent
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent] // which component is root component?
