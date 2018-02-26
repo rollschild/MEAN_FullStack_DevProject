e29818cc1cd8b5ec54b27f597f4f10e61a34b2d0
Guangchu Shi <rollschild@localhost.localdomain>
2018-02-26 15:12:50 -0500

    First commit

--- assets/app/messages/message-list.component.ts
+++ assets/app/messages/message-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { Message } from "./message.model";
+// import { MessageComponent } from "./message.component";
 @Component({
     selector: 'app-message-list',
     template: `
         <div class="col-md-8 col-md-offset-2">
             <app-message 
-                [message]="message" 
+                [inputMessage]="message" 
                 (editClicked)="message.content = $event" 
                 *ngFor="let message of messages">
                 </app-message>
